452b4476b9a73d73ae459bd88914f57a3a2d22cb
uday <udaymchavan228@gmail.com>
2024-07-16 02:38:58 +0530

    add functionality to the sign up page

--- api/index.js
+++ api/index.js
@@ -26,8 +26,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+app.listen(5173, () => {
+  console.log('Server is running on port 5173!');
 });
 
 app.use('/api/user', userRoutes);
